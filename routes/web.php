123bf20ec721cc2d088da7aa05518331622e2181
amida zulfa <amida.zulfa@upi.edu>
2025-03-20 22:17:26 +0700

    add routes portfolio, tentangkami, faqs

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,9 @@ use App\Http\Controllers\CaraOrderController;
 use App\Http\Controllers\TestimoniController;
 use App\Http\Controllers\KontakKamiController;
 use App\Http\Controllers\KategoriController;
+use App\Http\Controllers\TentangKamiController;
+use App\Http\Controllers\PortfolioController;
+use App\Http\Controllers\FaqsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -27,3 +30,6 @@ Route::get('/produk', [ProductController::class, 'index'])->name('produk.index')
 Route::get('/caraorder', [CaraOrderController::class, 'index'])->name('caraorder.index');
 Route::get('/testimoni', [TestimoniController::class, 'index'])->name('testimoni.index');
 Route::get('/kontakkami', [KontakKamiController::class, 'index'])->name('kontakkami.index');
+Route::get('/tentangkami', [TentangKamiController::class, 'index'])->name('tentangkami.index');
+Route::get('/portfolio', [PortfolioController::class, 'index'])->name('portfolio.index');
+Route::get('/faqs', [FaqsController::class, 'index'])->name('faqs.index');
